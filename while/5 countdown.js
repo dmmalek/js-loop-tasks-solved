e9c6840e7accddedaa2d587566f5f3f329a3180d
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-09-09 10:41:22 +0600

    solved countdown problem

--- while/5 countdown.js
+++ while/5 countdown.js
@@ -3,6 +3,12 @@
 Implement a countdown timer that counts down from 21 to 15.
 
  */
+let countdown = 21;
+while (countdown >= 15) {
+    console.log(countdown);
+    countdown--;
+}
+
 
 
 /*programming hero*/
