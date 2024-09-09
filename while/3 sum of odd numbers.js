3bdfc1609e7f8e15fd1afcdc2f5cd42bc114f094
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-09-09 10:08:05 +0600

    solved sum of odd number problem

--- while/3 sum of odd numbers.js
+++ while/3 sum of odd numbers.js
@@ -6,7 +6,18 @@ Subtask-1:
 
 Display sum of all the odd numbers from 81 to 131.
 
+
  */
+
+let num = 81;
+let sum = 0;
+while (num <= 131) {
+    console.log(num)
+    sum = sum + num;
+    num += 2;
+}
+
+console.log('Sum of odd number is :', sum);
 /***
 
 Subtask-2:
@@ -14,5 +25,13 @@ Subtask-2:
 Display sum of all the even numbers from 206 to 311.
 
  */
+let evenNum = 206;
+let evenSum = 0;
+while (evenNum <= 311) {
+    console.log(evenNum)
+    evenSum = evenSum + evenNum;
+    evenNum += 2;
+}
 
+console.log('Sum of Even number is :', evenSum);
 /*programming hero*/
